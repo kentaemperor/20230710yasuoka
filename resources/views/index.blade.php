e7483052f219da74cee6ec93c2a350cece88a0e4
kenta <bzmrchildren@icloud.com>
2022-12-17 19:29:48 +0900

    first-commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,36 +1,32 @@
-@extends('layouts.default')
-<style>
-  th {
-    background-color: #289ADC;
-    color: white;
-    padding: 5px 40px;
-  }
-  tr:nth-child(odd) td{
-    background-color: #FFFFFF;
-  }
-  td {
-    padding: 25px 40px;
-    background-color: #EEEEEE;
-    text-align: center;
-  }
-</style>
-@section('title', 'index.blade.php')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
 
-@section('content')
+<h1>Todo List</h1>
+
+<form action="/todos/create" method="post">
+  @csrf
+  <input type="text" name="content" >
+
+  <button type="button" name="name">追加</button>
+
+</form>
+
 <table>
-  <tr>
-    <th>id</th>
-    <th>name</th>
-    <th>age</th>
-    <th>nationality</th>
-  </tr>
-  @foreach ($authors as $author)
-  <tr>
-    <td>{{$author->id}}</td>
-    <td>{{$author->name}}</td>
-    <td>{{$author->age}}</td>
-    <td>{{$author->nationality}}</td>
-  </tr>
-  @endforeach
+<th>作成日</th>
+<th>タスク名</th>
+<th>更新</th>
+<th>削除</th>
+
 </table>
-@endsection
+
+
+</body>
+</html>
+
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\TodoController;
 
-Route::get('/', [AuthorController::class, 'index']);
+Route::get('/', [TodoController::class, 'index']);
 
-// 追記：ここから
-Route::get('/find', [AuthorController::class, 'find']);
-Route::post('/find', [AuthorController::class, 'search']);
-// 追記：ここまで
-
-// 追記：ここから
-Route::get('/add', [AuthorController::class, 'add']);
-Route::post('/add', [AuthorController::class, 'create']);
-// 追記：ここまで
-
-// 追記：ここから
-Route::get('/edit', [AuthorController::class, 'edit']);
-Route::post('/edit', [AuthorController::class, 'update']);
-// 追記：ここまで
-
-// 追記：ここから
-Route::get('/delete', [AuthorController::class, 'delete']);
-Route::post('/delete', [AuthorController::class, 'remove']);
-// 追記：ここまで
-
-Route::get('/author/{author}', [AuthorController::class, 'bind']);
+Route::post('/todos/create', [TodoController::class, 'store']);
