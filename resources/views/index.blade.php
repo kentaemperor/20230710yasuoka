726d337c9f473bc84c2aaf42512bccf24180a02b
kenta <bzmrchildren@icloud.com>
2023-01-07 21:55:04 +0900

    first-commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,71 +9,101 @@
 </head>
 <body>
 
+
+
   <div class="container">
 
     <div class="card">
-      
-      <p class="title mb-15">Todo List</p>
+
+      <h1 class="title">Todo List</h1>
+
             <div class="todo">
-              
-        <form action="/todo-introduction/todo/create" method="post" class="flex between mb-30">
-          @csrf
-          <input type="hidden" name="_token" value="70iTjYPmypqyJVcjyvyeHsqbNLFFc19NpZj8BHFq">          <input type="text" class="input-add" name="content">
-          <input class="button-add" type="submit" value="追加">
-        </form>
+
+            <form action="/todos/create" method="post">
+           @csrf
+
+            <input type="text" class="input-add" name="content">
+
+              <input class="button-add" type="submit" value="追加"></input> 
+
+
         <table>
-          <tbody><tr>
+         <tr>
             <th>作成日</th>
             <th>タスク名</th>
             <th>更新</th>
             <th>削除</th>
           </tr>
-                    <tr>
+
+
+           <tr>
             <td>
               2023-01-02 08:52:14
             </td>
-            <form action="http://54.65.181.123/todo-introduction/todo/update?id=143" method="post"></form>
-              <input type="hidden" name="_token" value="70iTjYPmypqyJVcjyvyeHsqbNLFFc19NpZj8BHFq">              <td>
-                <input type="text" class="input-update" value="プログラミング" name="content">
+
+            
+
+              <input type="hidden" name="_token" value="">
+
+               <td>
+                <input type="text" class="input-update" value="" name="content">
               </td>
+
               <td>
                 <button class="button-update">更新</button>
               </td>
             
             <td>
-              <form action="http://54.65.181.123/todo-introduction/todo/delete?id=143" method="post">
-                <input type="hidden" name="_token" value="70iTjYPmypqyJVcjyvyeHsqbNLFFc19NpZj8BHFq">                <button class="button-delete">削除</button>
-              </form>
+              
+                <input type="hidden" name="_token" value="">     
+                
+                <button class="button-delete">削除</button>
+              
             </td>
+
           </tr>
-                    <tr>
+
+          <tr>
+
             <td>
               2023-01-04 05:17:11
             </td>
-            <form action="http://54.65.181.123/todo-introduction/todo/update?id=146" method="post"></form>
-              <input type="hidden" name="_token" value="70iTjYPmypqyJVcjyvyeHsqbNLFFc19NpZj8BHFq">              <td>
-                <input type="text" class="input-update" value="筋トレ" name="content">
+
+            <td>
+
+             <input type="hidden" name="_token" value=""> 
+
+                <input type="text" class="input-update" value="" name="content">
+
               </td>
+
               <td>
                 <button class="button-update">更新</button>
               </td>
             
             <td>
-              <form action="http://54.65.181.123/todo-introduction/todo/delete?id=146" method="post">
-                <input type="hidden" name="_token" value="70iTjYPmypqyJVcjyvyeHsqbNLFFc19NpZj8BHFq">                <button class="button-delete">削除</button>
-              </form>
+
+              
+                <input type="hidden" name="_token" value="">
+
+                <button class="button-delete">削除</button>
+              
             </td>
+
           </tr>
-                  </tbody></table>
 
- @foreach ($todos as $todo)
-  <p>{{$todo->content}}</p>
-  @endforeach
+          </body>
 
+          </table>
+
+          @foreach ($todos as $todo)
+          <p>{{$todo->content}}</p>
+          @endforeach
       </div>
     </div>
   </div>
 
 </body>
+
 </html>
 
