836ffde03e3ba6f14feabba0a1d970d4ee14d989
kenta <bzmrchildren@icloud.com>
2023-01-20 14:49:05 +0900

    first-commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,104 +13,46 @@
 
 
   <div class="container">
-
     <div class="card">
       
-
       <h1 class="title">Todo List</h1>
-
-              <div class="todo">
-
-            <form action="/todos/create" method="post">
-           @csrf
-
-            <input type="text" class="input-add" name="content">
-
+      <div class="todo">
+        <form action="/todos/create" method="post">
+              @csrf
+              <input type="text" class="input-add" name="content"></input> 
               <input class="button-add" type="submit" value="追加"></input> 
 
+              <div class="Todo-table">      
+                <table class="Todo">
+                 <tr>
+                    <th>作成日</th>
+                    <th>タスク名</th>
+                    <th>更新</th>
+                    <th>削除</th>
+                  </tr>
 
-        <div class="Todo-table">      
+                 @foreach ($todos as $todo)
 
-        <table class="Todo">
+                  <tr>
+                    <td>{{$todo->id}}</td>
+                    <td>{{$todo->content}}</td>
+                    <td>{{$$todo->created_at}}</td>
+                    <td>{{$todo->updated_at}}</td>
+                  </tr>
 
-             <tr>
-                <th>作成日</th>
-                <th>タスク名</th>
-                <th>更新</th>
-                <th>削除</th>
-             </tr>
+                   @endforeach
 
-          @foreach ($todos as $todo)
 
-            <tr>
-              <td>{{$todo->id}}</td>
-              <td>{{$todo->content}}</td>
-              <td>{{$$todo->created_at}}</td>
-              <td>{{$todo->updated_at}}</td>
-             </tr>
-
-         @endforeach
-
-         <tr>
-            <td>
-              2023-01-02 08:52:14
-            </td>
-        
-              <input type="hidden" name="_token" value="">
-
-              <td>
-                <input type="text" class="input-update" value="" name="content">
-              </td>
-
-              <td>
-                <button class="button-update">更新</button>
-              </td>
-            
-             <td>
-              
-                <input type="hidden" name="_token" value="">     
-                
-                <button class="button-delete">削除</button>
-              
-             </td>
-
-          </tr>
-
-          <tr>
-
-            <td>
-              2023-01-04 05:17:11
-            </td>
-
-              <td>
-
-                <input type="hidden" name="_token" value=""> 
-
-                <input type="text" class="input-update" value="" name="content">
-
-              </td>
-
-              <td>
-                <button class="button-update">更新</button>
-              </td>
-            
-              <td>
-                <input type="hidden" name="_token" value="">
-
-                <button class="button-delete">削除</button>
-              
-              </td>
-
-          </tr>
-
-        </table> 
+                </table> 
       
-
                     @foreach ($todos as $todo)
-          <p>{{$todo->content}}</p>
-          @endforeach
+                   <p>{{$todo->content}}</p>
+                    @endforeach
+
+              </div>
+
       
-        </div>
+        </form>
         
       </div>
     </div>
